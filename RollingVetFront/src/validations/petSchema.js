d8966516e6e1b16728a539fd6b47f99dceb8baca
Alberto-Mainardi <albertomainardi.12@gmail.com>
2025-01-09 01:41:08 -0300

    validacion del nombre al crear o modificar mascota con zod

--- RollingVetFront/src/validations/petSchema.js
+++ RollingVetFront/src/validations/petSchema.js
@@ -1,7 +1,11 @@
 import {z} from 'zod';
 
-export const petSchema = z.object({
-    nombre: z.string().min(2,{message: "El nombre de la mascota debe contener al menos 2 caracteres"}),
-    especia: z.
+const regexNombre = /^[A-Za-z]+$/;
 
-})
+export const petSchema = z.object({
+    nombre: z.string().min(2,{message: "El nombre de la mascota debe contener al menos 2 caracteres"}).max(25, {message: "El nombre de la mascota no debe superar los 25 caracteres"}).regex(regexNombre, {
+        message: "El nombre solo debe incluir letras",
+      }),
+    especie: z.string().refine(async (especie) => {validarEspecie(especie)})
+        
+    })
