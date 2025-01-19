973a7e0505e92f8386d03497bb64c02c97a9fe49
FelipeSarco17 <sarcofelipe456@gmail.com>
2025-01-19 18:24:21 -0300

    Correcion de errores provacadas por merge en form registrarTurno

--- RollingVetFront/src/components/FormComponents/Select.jsx
+++ RollingVetFront/src/components/FormComponents/Select.jsx
@@ -1,6 +1,6 @@
 import React, { forwardRef } from "react";
 
-const Select = forwardRef(({ name, options, register, error, label, ...rest }, ref) => {
+const Select = ({ name, options, register, error, label, ...rest }, ref) => {
     
   return (
     <div className="mb-4">
@@ -8,20 +8,19 @@ const Select = forwardRef(({ name, options, register, error, label, ...rest }, r
       <select
         id={name}
         {...register(name)}
-        ref={ref}
         {...rest}
         className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
       >
         <option value="">Selecciona una opción</option>
         {options.map((option, index) => (
-          <option key={index} value={option.value}>
-            {option.label}
+          <option key={index} value={option}>
+            {option}
           </option>
         ))}
       </select>
       {error && <p className="text-red-600 text-sm mt-1">{error}</p>}
     </div>
   );
-});
+}
 
 export default Select;
