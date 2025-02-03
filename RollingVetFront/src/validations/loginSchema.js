7e57e63858c36be312d21d38aece69cdce9f8b52
FelipeSarco17 <sarcofelipe456@gmail.com>
2025-02-03 15:04:18 -0300

    Validaciones en form iniciar sesion arregladas

---
+++ RollingVetFront/src/validations/loginSchema.js
@@ -0,0 +1,9 @@
+import {z} from "zod"
+
+
+export const loginSchema = z.object({
+
+    email: z.string({required_error:"El email es requerido"}).email({message:"Ingrese un email valido"}),
+    clave:z.string({required_error:"La contraseña es requerida"})
+
+})
--- RollingVetFront/src/views/IniciarSesion.jsx
+++ RollingVetFront/src/views/IniciarSesion.jsx
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthProvider'
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
+import Input from '../components/FormComponents/Input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { loginSchema } from '../validations/loginSchema';
 
 const IniciarSesion = () => {
 
   const { validarUsuario, user } = useAuth();
   const navigate = useNavigate()
-  const { register, handleSubmit, formState: { errors }, reset } = useForm()
+  const { register, handleSubmit, formState: { errors }, reset } = useForm({resolver:zodResolver(loginSchema)})
 
   const onSubmit = async (obj) => {
     
@@ -33,46 +36,10 @@ const IniciarSesion = () => {
 
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form action="#" method="POST" className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    required
-                    autoComplete="email"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    {...register("email")}
-                  />
-                </div>
-              </div>
-
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Contraseña
-                  </label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-rose-600 hover:text-indigo-500">
-                      Olvidaste tu contraseña?
-                    </a>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    autoComplete="current-password"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    {...register("password")}
-                  />
-                </div>
-              </div>
+              
+              <Input type="text" error={errors.email?.message} label="Email" name="email" register={register}/>
+              <Input type="password" error={errors.clave?.message} label="Contraseña" name="clave" register={register}/>
+              
 
               <div>
                 <button
